1caec0c5be4caa42e5342e210b6a170af0a86e1b
Eldos <eldos.baktybekov@gmail.com>
2025-05-29 01:43:31 +0600

    feat: define custom theme preset for PrimeVue in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primeuix/themes/aura";
+import { definePreset } from '@primeuix/themes';
 import tailwindcss from "@tailwindcss/vite";
+
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{orange.50}',
+            100: '{orange.100}',
+            200: '{orange.200}',
+            300: '{orange.300}',
+            400: '{orange.400}',
+            500: '{orange.500}',
+            600: '{orange.600}',
+            700: '{orange.700}',
+            800: '{orange.800}',
+            900: '{orange.900}',
+            950: '{orange.950}'
+        }
+    }
+});
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
@@ -15,7 +35,7 @@ export default defineNuxtConfig({
   primevue: {
     options: {
       theme: {
-        preset: Aura,
+        preset: MyPreset,
       },
       locale: {
         dayNames: [
