08b3b4c34187d87b9df9557e5511cf1ef87925ba
Eldos <eldos.baktybekov@gmail.com>
2025-05-29 02:48:57 +0600

    feat: customize NuxtLoadingIndicator color for enhanced visual appeal

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtLoadingIndicator />
+    <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #ff8000 0%, #ff9a00 50%, #ffc300 100%)"/>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
