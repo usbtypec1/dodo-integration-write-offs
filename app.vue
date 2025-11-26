000db987cc5edb44515162d39880adb307ce12e2
Eldos <eldos.baktybekov@gmail.com>
2025-11-26 21:28:06 +0600

    feat: wrap Nuxt components in ClientOnly for improved rendering

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #ff8000 0%, #ff9a00 50%, #ffc300 100%)"/>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <ClientOnly>
+      <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #ff8000 0%, #ff9a00 50%, #ffc300 100%)"/>
+      <NuxtLayout>
+        <NuxtPage/>
+      </NuxtLayout>
+    </ClientOnly>
   </div>
 </template>
